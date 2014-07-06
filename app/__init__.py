7134934f2e3f3755968769fdb88030a2ee768b1f
Jackson Owens <jackson_owens@brown.edu>
2014-07-06 12:57:07 -0700

    added images and css styling

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='', static_folder='static')
 
 app.config.from_pyfile('../config/default.cfg')
 
@@ -10,9 +10,9 @@ from blueprints.main import main
 app.register_blueprint(main)
 
 # Serve some static files too
-@app.route('/css/<path:path>')
-def static_server(path):
-    full_path = os.path.join('static', 'css', path)
-    app.logger.debug('Looking for path %s', full_path)
-    app.logger.debug(os.path.isfile(full_path))
-    return app.send_static_file(full_path)
+# @app.route('/css/<path:path>')
+# def static_server(path):
+#     full_path = os.path.join('static', 'css', path)
+#     app.logger.debug('Looking for path %s', full_path)
+#     app.logger.debug(os.path.isfile(full_path))
+#     return app.send_static_file(full_path)
