a6a469ae28461915642bb1fb5d11192c32c9511a
Jackson Owens <jackson_owens@brown.edu>
2014-07-06 12:45:26 -0700

    started porting html

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
+import os
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 app.config.from_pyfile('../config/default.cfg')
 
 # Setup blueprints
 from blueprints.main import main
 app.register_blueprint(main)
+
+# Serve some static files too
+@app.route('/css/<path:path>')
+def static_server(path):
+    full_path = os.path.join('static', 'css', path)
+    app.logger.debug('Looking for path %s', full_path)
+    app.logger.debug(os.path.isfile(full_path))
+    return app.send_static_file(full_path)
